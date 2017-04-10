362398dc7599d9a26b3cc5d43664881aaa4a05fe
Sandro Mijatovic <mija.sand@gmail.com>
2017-04-10 13:14:15 -0700

    this commit will add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { routing } from './app.routing';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2I6gd65QWEY4ZsMIy8lSJv1xZy1whKOc",
+  authDomain: "pickup-game-754fd.firebaseapp.com",
+  databaseURL: "https://pickup-game-754fd.firebaseio.com",
+  projectId: "pickup-game-754fd",
+  storageBucket: "pickup-game-754fd.appspot.com",
+  messagingSenderId: "130415141763"
+};
+
 
 @NgModule({
   declarations: [
@@ -15,7 +27,8 @@ import { routing } from './app.routing';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
