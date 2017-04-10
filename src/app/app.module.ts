dbe67e431cc6b52f70e2d1a6e56d02c7cdfc2ce8
mattcaswell <matthewcaswell253@Gmail.com>
2017-04-10 16:13:51 -0700

    created basketball court API for seattle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { routing } from './app.routing';
+
 
 @NgModule({
   declarations: [
