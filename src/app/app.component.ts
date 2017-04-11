f7bd0323f56be35451cd1368d46bcf766283b8f4
bcooper085 <bcooper085@gmail.com>
2017-04-11 16:35:28 -0700

    add twitter search function on init, moved to account page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
-const Twit = require('twit');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent {
   title = 'Welcome to Pick-Up-Games!';
   link = 'https://data.seattle.gov/resource/3c4b-gdxv.json?$$app_token=MTNJ2m540epjQwhEl5o9Bjc3q';
   http: Http;
   courts = [];
-  private T;
 
   constructor(http: Http) {
     this.http = http;
@@ -30,13 +28,4 @@ export class AppComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  this.T = new Twit({
-    consumer_key:         'oypNKIloGeMbadqaL72MU20yk',
-    consumer_secret:      'zPRe2pPm5ViDXTuZTXXTVeVaJorh3gbAL25Wsi43s8ftvuqxUn',
-    access_token:         '851854992117911552-zBQBHhoSbthU0cXxAikBDIP6YLgpTiS',
-    access_token_secret:  'XJxuq6Fs2Vvk6wQMndQwOiNFRhC39RIvITa3Uwoc6Qdz8',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    })
-  }
 };
