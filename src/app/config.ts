a7a3be6a4159ae4ac14231cc0bbcba58dbf0caa9
bcooper085 <bcooper085@gmail.com>
2017-04-12 14:37:23 -0700

    add base styling to home page for new game form, twitter feed and twitter search

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,7 @@
-var appsettings = {
+export var appSettings = {
   consumer_key:         'oypNKIloGeMbadqaL72MU20yk',
   consumer_secret:      'zPRe2pPm5ViDXTuZTXXTVeVaJorh3gbAL25Wsi43s8ftvuqxUn',
   access_token:         '851854992117911552-zBQBHhoSbthU0cXxAikBDIP6YLgpTiS',
   access_token_secret:  'XJxuq6Fs2Vvk6wQMndQwOiNFRhC39RIvITa3Uwoc6Qdz8',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
-
-module.exports = appsettings;
--- src/app/twitter.service.ts
+++ src/app/twitter.service.ts
@@ -1,18 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers} from '@angular/http'
+import { Http, Headers} from '@angular/http';
 
 @Injectable()
 export class TwitterService {
 
   constructor(private http: Http) { }
 
-  makecall() {
-    var headers = new Headers();
-
-    headers.append('basketball', 'application/X-www-form-urlencoded');
-
-    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
-      console.log(res);
-    })
-  }
 }
